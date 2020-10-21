cedfebe557fc9d77f5d88ec299f8ab8d5c54719a
gitu30 <73221975+gitu30@users.noreply.github.com>
2020-10-21 17:11:20 +0530

    Add files via upload

---
+++ Amezon.java
@@ -0,0 +1,20 @@
+package trial1;
+
+public class Amezon {
+	double product;
+	double price;
+	public double getProduct() {
+		return product;
+	}
+	public void setProduct(double product) {
+		this.product = product;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
